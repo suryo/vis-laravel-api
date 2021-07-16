08e6cce66e96126cf1230249becbec6126df69d4
suryo <suryoatm@gmail.com>
2021-07-16 21:17:06 +0700

    mod route

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,15 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+// Mengambil Satu kabupaten dengan provinsi
+Route::get('/kabupaten/all', 'App\Http\Controllers\Api\KabupatenController@showWithProvinsi');
+// Mengambil Satu kabupaten dengan provinsi
+Route::get('/kabupaten/all/{id}', 'App\Http\Controllers\Api\KabupatenController@showWithProvinsibyId');
+
 Route::apiResource('/provinsi', App\Http\Controllers\Api\ProvinsiController::class);
+
+Route::apiResource('/kabupaten', App\Http\Controllers\Api\KabupatenController::class);
+
+Route::apiResource('/datapenduduk', App\Http\Controllers\Api\DataPendudukController::class);
+
+Route::apiResource('/jenislembagadesa', App\Http\Controllers\Api\JenisLembagaDesaController::class);

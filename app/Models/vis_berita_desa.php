2a7ab6991f05a9508588ae751472030c522151af
suryo <suryoatm@gmail.com>
2021-07-17 21:27:13 +0700

    mod model berita desa

--- app/Models/vis_berita_desa.php
+++ app/Models/vis_berita_desa.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class vis_berita_desa extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'id_desa', 'judul', 'tanggal', 'berita'
+    ];
+
 }
+

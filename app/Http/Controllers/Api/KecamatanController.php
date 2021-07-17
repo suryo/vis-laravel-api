5072416611967c33e5344e906f15412833053435
suryo <suryoatm@gmail.com>
2021-07-17 21:41:48 +0700

    mod controller kecamatan

--- app/Http/Controllers/Api/KecamatanController.php
+++ app/Http/Controllers/Api/KecamatanController.php
@@ -2,10 +2,10 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Models\vis_kabupaten;
+use App\Models\vis_kecamatan;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Http\Resources\KabupatenResource;
+use App\Http\Resources\KecamatanResource;
 use Illuminate\Support\Facades\Validator;
 use DB;
 
@@ -18,7 +18,7 @@ class KecamatanController extends Controller
      */
     public function index()
     {
-        return new KabupatenResource(vis_kabupaten::all());
+        return new KecamatanResource(vis_kecamatan::all());
     }
 
     /**
@@ -31,8 +31,10 @@ class KecamatanController extends Controller
     {
         //set validation
         $validator = Validator::make($request->all(), [
+            'id_kota' => 'required',
+            'id_kabupaten' => 'required',
             'id_provinsi' => 'required',
-            'kabupaten'   => 'required'
+            'kecamatan'   => 'required'
         ]);
 
         //response error validation
@@ -41,36 +43,38 @@ class KecamatanController extends Controller
         }
 
         //save to database
-        $kabupaten = vis_kabupaten::create([
+        $kecamatan = vis_kecamatan::create([
+            'id_kota'     => $request->id_kota,
+            'id_kabupaten'     => $request->id_kabupaten,
             'id_provinsi'     => $request->id_provinsi,
-            'kabupaten'     => $request->kabupaten
+            'kecamatan'     => $request->kecamatan
         ]);
 
-        return new KabupatenResource($kabupaten);
+        return new KecamatanResource($kecamatan);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  vis_kabupaten $kabupaten
+     * @param  vis_kecamatan $kecamatan
      * @return \Illuminate\Http\Response
      */
-    public function show(vis_kabupaten $kabupaten)
+    public function show(vis_kecamatan $kecamatan)
     {
-        return new KabupatenResource($kabupaten);
+        return new KecamatanResource($kecamatan);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  vis_kabupaten $kabupaten
+     * @param  vis_kecamatan $kecamatan
      * @return \Illuminate\Http\Response
      */
-    public function showWithProvinsi(vis_kabupaten $kabupaten)
+    public function showWithProvinsi(vis_kecamatan $kecamatan)
     {
         //dd("asik");
-        $result =  DB::table('vis_kabupatens')
-        ->join('vis_provinsis', 'vis_provinsis.id', '=', 'vis_kabupatens.id_provinsi')
+        $result =  DB::table('vis_kecamatans')
+        ->join('vis_provinsis', 'vis_provinsis.id', '=', 'vis_kecamatans.id_provinsi')
         ->get();
         return response()->json(['data'=>$result]);
     }
@@ -78,20 +82,20 @@ class KecamatanController extends Controller
      /**
      * Display the specified resource.
      * @param  \Illuminate\Http\Request  $request
-     * @param  vis_kabupaten $kabupaten
+     * @param  vis_kecamatan $kecamatan
      * @return \Illuminate\Http\Response
      */
     public function showWithProvinsibyId(Request $request)
     {
         //dd($request->id);
-        // $result =  DB::table('vis_kabupatens')
-        // ->select('vis_kabupatens.*',
-        // DB::raw('(select provinsi from vis_provinsis where id = vis_kabupatens.id_provinsi ) as provinsi')
+        // $result =  DB::table('vis_kecamatans')
+        // ->select('vis_kecamatans.*',
+        // DB::raw('(select provinsi from vis_provinsis where id = vis_kecamatans.id_provinsi ) as provinsi')
         // )
-        // ->where('vis_kabupatens.id', $request->id)        
+        // ->where('vis_kecamatans.id', $request->id)        
         // ->get();
 
-        $result =  DB::select('select * from vis_kabupatens as vk 
+        $result =  DB::select('select * from vis_kecamatans as vk 
         JOIN vis_provinsis as vp 
         ON 
         vk.id_provinsi=vp.id 
@@ -103,15 +107,17 @@ class KecamatanController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  vis_kabupaten $kabupaten
+     * @param  vis_kecamatan $kecamatan
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, vis_kabupaten $kabupaten)
+    public function update(Request $request, vis_kecamatan $kecamatan)
     {
         //set validation
         $validator = Validator::make($request->all(), [
-            'id_provinsi'   => 'required',
-            'kabupaten'   => 'required'
+            'id_kota' => 'required',
+            'id_kabupaten' => 'required',
+            'id_provinsi' => 'required',
+            'kecamatan'   => 'required'
         ]);
 
         //response error validation
@@ -120,24 +126,26 @@ class KecamatanController extends Controller
         }
 
         //update to database
-        $kabupaten->update([
-            'id_provinsi' => $request->id_provinsi,
-            'kabupaten'     => $request->kabupaten
+        $kecamatan->update([
+            'id_kota'     => $request->id_kota,
+            'id_kabupaten'     => $request->id_kabupaten,
+            'id_provinsi'     => $request->id_provinsi,
+            'kecamatan'     => $request->kecamatan
         ]);
 
-        return new KabupatenResource($kabupaten);
+        return new KecamatanResource($kecamatan);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  vis_kabupaten $kabupaten
+     * @param  vis_kecamatan $kecamatan
      * @return \Illuminate\Http\Response
      */
-    public function destroy(vis_kabupaten $kabupaten)
+    public function destroy(vis_kecamatan $kecamatan)
     {
-        $kabupaten->delete();
+        $kecamatan->delete();
         
-        return new KabupatenResource($kabupaten);
+        return new KecamatanResource($kecamatan);
     }
 }

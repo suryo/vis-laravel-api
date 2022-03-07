6250a5b73f9fdfe38dc1e66acf7fa93388670ccb
suryo <suryoatm@gmail.com>
2022-03-07 16:17:23 +0700

    auth

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Http\Resources\User as UserResource;
+use Illuminate\Http\Request;
+use App\Http\Requests\ValidateUserRegistration;
+use App\Http\Requests\ValidateUserLogin;
+use App\User;
+
+
+class AuthController extends Controller
+{
+    //
+}
